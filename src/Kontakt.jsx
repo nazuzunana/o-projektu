77045e7a026525d7ac7f3559f994e53ecb6150c2
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-05-20 00:05:02 +0200

    pridani dalsi stranky Kontakt

---
+++ src/Kontakt.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Kontakt = () => {
+  return (
+    <div className="card">
+      <h2>Kontakt</h2>
+      <div className="card__text">
+        <p>Odkaz na repo na Githube alebo e-mail.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Kontakt;
--- src/index.jsx
+++ src/index.jsx
@@ -5,17 +5,19 @@ import './style.css';
 import Projekt from './Projekt';
 import Mydve from './Mydve';
 import Main from './Main';
+import Kontakt from './Kontakt';
 
 const App = () => (
   <div className="container">
     <header>
-      <h1>O projektu Booklog</h1>
+      <h1>O projektu dobročinných aukcí</h1>
     </header>
     <main>
       <nav>
         <Link to="/">Hlavní stránka</Link>
         <Link to="/projekt">O projektu</Link>
         <Link to="/mydve">Vik a Zuz</Link>
+        <Link to="/kontakt">Kontakt</Link>
       </nav>
       <Outlet />
     </main>
@@ -32,6 +34,7 @@ createRoot(document.querySelector('#app')).render(
         <Route path="/" element={<Main />} />
         <Route path="projekt" element={<Projekt />} />
         <Route path="mydve" element={<Mydve />} />
+        <Route path="kontakt" element={<Kontakt />} />
       </Route>
     </Routes>
   </BrowserRouter>,
